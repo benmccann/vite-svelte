090185149a4ae99f0085278bf543149935bf4c0e
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-01-21 22:06:32 -0800

    Set generate option to build SSR build

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import svelte from 'rollup-plugin-svelte'
 
 const production = (process.env.NODE_ENV === 'production')
 
+// TODO: this is hacky
+const ssr = JSON.stringify(process.env).includes('--ssr');
+
 export default {
   plugins: [
     svelte({
       emitCss: false,
       compilerOptions: {
         dev: !production,
+        generate: ssr ? 'ssr' : 'dom'
       }
     })
   ],
