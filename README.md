89d0c46bb0887695c21ad984b3f72d661a0c93cc
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-01-22 14:06:38 -0800

    Switch to vite-plugin-svelte

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+An example Svelte project on Vite
+
+Requires [vite-plugin-svelte](https://github.com/benmccann/vite-plugin-svelte)
+
 Run `npm run dev` for dev mode
 
 Run `npm run build` and then `node server` for SSR mode
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
-import svelte from 'rollup-plugin-svelte'
+import svelte from 'vite-plugin-svelte'
 
 const production = (process.env.NODE_ENV === 'production')
-
-// TODO: this is hacky
-const ssr = JSON.stringify(process.env).includes('--ssr');
 
 export default {
   plugins: [
     svelte({
       emitCss: false,
       compilerOptions: {
-        dev: !production,
-        generate: ssr ? 'ssr' : 'dom'
+        dev: !production
       }
     })
-  ],
-  optimizeDeps: {
-  	exclude: ['svelte']
-  }
+  ]
 }
