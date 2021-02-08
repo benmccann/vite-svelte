8c7c37496ad2dab786782f94250a579730efd0fc
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-02-08 09:37:53 -0800

    Fix usage of third-party svelte libraries

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ const production = (process.env.NODE_ENV === 'production')
 
 export default {
 	plugins: [
-		svelte({
-			emitCss: false,
-			compilerOptions: {
-				dev: !production,
-				hydratable: true
-			}
-		})
+		{
+			...svelte({
+				emitCss: false,
+				compilerOptions: {
+					dev: !production,
+					hydratable: true
+				}
+			}),
+			// Make our plugin handle resolveId before the vite internal resolver
+			// This allows us to handle third-party svelte libraries
+			enforce: 'pre'
+		}
 	],
 	ssr: {
 		noExternal: ['svelte-awesome']
